eb720c2dee7f62d856958c1557a71c215de980f1
Thalita Machado <61170238+Thalita1044@users.noreply.github.com>
2022-02-24 16:09:32 -0300

    Update exerc1.js

--- exerjs - Copia/exerc1.js
+++ exerjs - Copia/exerc1.js
@@ -1,8 +1,8 @@
-/*
-Funções Simples
+
+/*Funções Simples
 1. Crie uma função que converta polegadas em centímetros.
 Recebe pelo parâmetro polegadas e retorna seu equivalente em centímetros.
-
+*/
 const polParaCm = (pol) => {
   return pol * 2.54;
 }
@@ -10,18 +10,18 @@ const polParaCm = (pol) => {
 console.log(polParaCm(5)) ;
 
 
-2. Crie uma função que receba uma string e a converta em um URL.
+/*2. Crie uma função que receba uma string e a converta em um URL.
 ex: "funcaojs" é retornado como "http://www.funcaojs.com.br"
-
+*/
 
 const stringParaUrl = (string) => {
   return `http://www.${string}.com.br`;
 }
 stringParaUrl('google');
 
-3. Crie uma função que recebe uma string e retorna a mesma frase, mas com o
+/*3. Crie uma função que recebe uma string e retorna a mesma frase, mas com o
 caracter de exclamação ( ! ).
-
+*/
 
 const frase = (string) => {
 return `${string}!` ;
@@ -29,11 +29,11 @@ return `${string}!` ;
 frase("essa atividade foi facil");
 
 
-4. Crie uma função que calcule a idade dos cachorros, considerando que 1 ano
+/*4. Crie uma função que calcule a idade dos cachorros, considerando que 1 ano
 humano, equivale a 7 anos para eles.
-
+*/
 
 const dog = (idade) =>{
   return idade * 7;
 {
-*/
+
